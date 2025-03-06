1e35b5d8d71a8cb2093d6e2534c81fb7d8b49331
brightDN <brightondeneef@gmail.com>
2025-03-06 23:23:49 +0100

    commiting

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import shutil
+import sys
 from src.file_operations.move_from_static_to_public import move_from_static_to_public
 from src.file_operations.generate_page import generate_page
 from src.file_operations.generate_pages import generate_pages
 
-def main():
+def main(basepath = "/"):
+    sys.argv
 
     if os.path.exists("public"):
         shutil.rmtree("public")
@@ -14,7 +16,10 @@ def main():
     if os.path.exists("static"):
         move_from_static_to_public("static", "public")
     # generate_page("content/index.md", "template.html", "public/index.html")
-    generate_pages("content", "template.html", "public")
+    generate_pages("content", "template.html", "public", basepath)
 
 if __name__ == "__main__":
-    main()
+    basepath = '/'
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    main(basepath)
--- src/file_operations/generate_pages.py
+++ src/file_operations/generate_pages.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from src.file_operations.generate_page import generate_page
 
-def generate_pages(source_path: str, template_file_path: str, destination_path: str) -> None:
+def generate_pages(source_path: str, template_file_path: str, destination_path: str, basepath: str) -> None:
     """
     This function takes in 2 parameters and moves all files and directories from the source_path parameter into the destionation_path parameter.
 
@@ -23,7 +23,7 @@ def generate_pages(source_path: str, template_file_path: str, destination_path: 
         
         if os.path.isfile(source_item_path):
             dest_item_path = os.path.join(destination_path, item.replace('.md', '.html'))
-            generate_page(source_item_path, template_file_path, dest_item_path)
+            generate_page(source_item_path, template_file_path, dest_item_path, basepath)
         else:
             # Handle directories
             dest_item_path = os.path.join(destination_path, item)
@@ -31,4 +31,4 @@ def generate_pages(source_path: str, template_file_path: str, destination_path: 
             if not os.path.exists(dest_item_path):
                 os.mkdir(dest_item_path)
             # Recursively process the subdirectory
-            generate_pages(source_item_path, template_file_path, dest_item_path)
+            generate_pages(source_item_path, template_file_path, dest_item_path, basepath)
